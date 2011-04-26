9156dfe410cd9080cc6b5c36c6344e484917a264
Eric Nelson <eric.mckenzie.nelson@gmail.com>
2011-04-25 19:26:33 -0700

    yes

--- lib/intuit_test.rb
+++ lib/intuit_test.rb
@@ -72,6 +72,26 @@ post '*' do
 
   # Display customer list in the Intuit Workplace iframe
   "Complete"
-  #erb :index
+  erb :index
 end
+######
+__END__
+
+@@ layout
+<html>
+  <body>
+	<%= yield %>
+  </body>
+</html>
+
+@@ index
+<h1>Customers</h1>
+<ul>
+  <% for customer in @customers %>
+	<li><%= h customer.name %></li>
+  <% end %>
+</ul>
+
+######
+
 end  
